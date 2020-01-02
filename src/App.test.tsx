de0b47db09d64707e80a57dddbe8dc606f3d0e4b
Yevstakhii Krul <yevstakhii.krul@sombrainc.com>
2020-01-02 12:45:22 +0200

    tests for login modal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { LocalStorageMock } from '@react-mock/localstorage';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders login link without token', () => {
+  const { getByText } = render(
+    <LocalStorageMock>
+      <App />
+    </LocalStorageMock>
+  );
+  const linkElement = getByText(/Login using github/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("doesn't render login link with token", () => {
+  const { queryByText } = render(
+    <LocalStorageMock items={{ token: 'mocked token' }}>
+      <App />
+    </LocalStorageMock>
+  );
+  const linkElement = queryByText(/Login using github/i);
+  expect(linkElement).not.toBeInTheDocument();
 });
