fd4f8833bd7b6fb6c78ac3a5823709d6376bd7d5
Yevstakhii Krul <yevstakhii.krul@sombrainc.com>
2020-01-20 15:56:47 +0200

    offline first, pwa

--- src/components/UsersList/UsersList.test.tsx
+++ src/components/UsersList/UsersList.test.tsx
@@ -6,29 +6,12 @@ import UsersList from 'components/UsersList/UsersList';
 
 const mocks = [
   {
-    request: { query: USERS_AVATARS_QUERY },
+    request: { query: USERS_AVATARS_QUERY, variables: { login: 'sombra-yuriy' } },
     result: {
       data: {
-        sombraPavloDaniv: {
-          avatarUrl: 'https://avatars1.githubusercontent.com/u/30520581?s=55&v=4',
-        },
-        sombraDmytroMula: {
-          avatarUrl: 'https://avatars0.githubusercontent.com/u/30520298?s=55&v=4',
-        },
-        sombraYuriy: {
+        user: {
           avatarUrl: 'https://avatars2.githubusercontent.com/u/43397550?s=55&v=4',
-        },
-        sombraOstryzhniukAndrii: {
-          avatarUrl: 'https://avatars2.githubusercontent.com/u/32451407?s=55&v=4',
-        },
-        sombraRomanPolagiv: {
-          avatarUrl: 'https://avatars3.githubusercontent.com/u/30521980?s=55&v=4',
-        },
-        sombraNazarOstryzhniuk: {
-          avatarUrl: 'https://avatars2.githubusercontent.com/u/49642404?s=55&v=4',
-        },
-        sombraIvanArabchuk: {
-          avatarUrl: 'https://avatars0.githubusercontent.com/u/30521399?s=55&v=4',
+          login: 'sombra-yuriy',
         },
       },
     },
--- src/queries/user.ts
+++ src/queries/user.ts
@@ -1,18 +1,10 @@
 import gql from 'graphql-tag';
-import users from 'data/users.json';
-import camelCase from 'lodash/camelCase';
 
 export const USERS_AVATARS_QUERY = gql`
-  query usersQuery {
-    ${users.map(
-      // github doesn't support batched requests and kebab-case keys, so maping through the logins
-      // and turning them to camelCase is the only way to avoid sending all the requests at once
-      user => `
-      ${camelCase(user)}: user(login: "${user}") {
-        avatarUrl(size: 55)
-      }
-    `
-    )}
+  query usersAvatarsQuery($login: String!) {
+    user(login: $login) {
+      avatarUrl(size: 55)
+    }
   }
 `;
 
