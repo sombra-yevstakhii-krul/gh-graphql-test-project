854f4a702f2082be3d0d9ee26ce3229753768a9c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-31 11:10:47 +0000

    Deploy sombra-yevstakhii-krul/gh-graphql-test-project to github.com/sombra-yevstakhii-krul/gh-graphql-test-project.git:gh-pages

---
+++ precache-manifest.1de4542df8bbb24633e9e424f4c81fe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdd3dee1e8216c2508f93bdc5bcdaf9",
+    "url": "/gh-graphql-test-project/index.html"
+  },
+  {
+    "revision": "b1244a10bc2f82d5829d",
+    "url": "/gh-graphql-test-project/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "b1244a10bc2f82d5829d",
+    "url": "/gh-graphql-test-project/static/js/2.077a054c.chunk.js"
+  },
+  {
+    "revision": "11ac391511c99f196b2d7aa5eecda4ec",
+    "url": "/gh-graphql-test-project/static/js/2.077a054c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7aac4c73a1317f411990",
+    "url": "/gh-graphql-test-project/static/js/main.93e99b22.chunk.js"
+  },
+  {
+    "revision": "65f32ecda4f35e2f7167",
+    "url": "/gh-graphql-test-project/static/js/runtime-main.c675d3f2.js"
+  },
+  {
+    "revision": "5b9a3215a9953c2630cb7ab45ab3edac",
+    "url": "/gh-graphql-test-project/static/media/github-icon.5b9a3215.svg"
+  }
+]);
